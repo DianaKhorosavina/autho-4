3843cf50aeda7674af3866aac3a18e71324d8e5c
Diana Khorosavina <dkhorosavina@gmail.com>
2023-12-19 13:40:29 +0300

    card delivery

---
+++ src/test/java/ru/netology/autho/CardDeliveryTest.java
@@ -0,0 +1,54 @@
+package ru.netology.autho;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    public void openAll() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+
+    void SuccessfullyCompletedTest(){
+        $("[data-test-id='city'] input").setValue("Москва");
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Петров Александр");
+        $("[data-test-id='phone'] input").setValue("+79011479956");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " +planningDate));
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
